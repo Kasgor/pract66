7f527b0926a0ff05500b122aa933cbf46ad78918
Kasgor <shadow.crst@gmail.com>
2024-06-23 17:33:58 +0300

    Broken cucumber

---
+++ src/test/java/steps/CucumberTest.java
@@ -0,0 +1,12 @@
+package steps;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+public class CucumberTest {
+}
